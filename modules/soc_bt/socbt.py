7cdbb1f85040cad297ee455e331376e7ae56b51f
nikigraus <dani@reislimann.ch>
2019-09-16 12:15:45 +0200

    Update socbt.py

--- modules/soc_bt/socbt.py
+++ modules/soc_bt/socbt.py
@@ -1,1 +1,17 @@
 
+#!/usr/bin/env
+import sys
+import time
+
+
+leaftimer = open('/var/www/html/openWB/ramdisk/soctimer', 'r')
+#leaftimer = int(leaftimer.read())
+#thecommand=str('python3 /var/www/html/openWB/modules/soc_leaf/getsoc.py')
+#thestring=str( sys.argv[1] + ' ' + sys.argv[2] + ' &') 
+leaftimer += 1
+f = open('/var/www/html/openWB/ramdisk/soctimer', 'w')
+f.write(str(leaftimer))
+f.close()
+f = open('/var/www/html/openWB/ramdisk/soc', 'w')
+f.write(str(leaftimer))
+f.close()
