f5b854bee521dfe088cabc5baf39ad443c24327d
nikigraus <dani@reislimann.ch>
2019-09-18 15:40:09 +0200

    implemented choice for scan / non-scan mode

--- modules/soc_bt/socbt.py
+++ modules/soc_bt/socbt.py
@@ -9,39 +9,56 @@ from bluepy import btle
 from bluepy.btle import UUID, Peripheral
 from bluepy.btle import Scanner, DefaultDelegate
 
+# ----------------------------------------------------------------------------------------
+# arg 1: MAC address
+# arg 2: CAN Telegram COBID
+# arg 3: CAN Telegram byte number (0-7)
+# arg 4: Value offset 
+# arg 5: Value factor (float value)
+# ----------------------------------------------------------------------------------------
+# Module will return the selected byte from the CAN telegram, treated by offset and factor
+# SOC = (byteval + offset) * factor
+# ----------------------------------------------------------------------------------------
+
 thestring=str(sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + sys.argv[4] + ' ' + sys.argv[5])
 print("soc_bt: " + thestring)
 
-g_cobid = "0374"
-g_val = 0
+g_strMACAddress = sys.argv[1]
+g_strCOBID = "0374"
+g_intVal = 0
+g_exit_code = 0
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
+#class ScanDelegate(DefaultDelegate):
+#    def __init__(self):
+#        DefaultDelegate.__init__(self)
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device", dev.addr)
-        elif isNewData:
-            print("Received new data from", dev.addr)
+#    def handleDiscovery(self, dev, isNewDev, isNewData):
+#        if isNewDev:
+#            print("Discovered device", dev.addr)
+#        elif isNewData:
+#            print("Received new data from", dev.addr)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-#scanner = Scanner()
-devices = scanner.scan(2.0)
-devaddr = 0
-exit_code = 0
+#scanner = Scanner().withDelegate(ScanDelegate())
 
-for dev in devices:
-#    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-    devName = dev.getValueText(9)
-    if devName  == "dani" :
-        print("found dani device with addr: " + dev.addr)
-        devaddr = dev.addr
-#    for (adtype, desc, value) in dev.getScanData():
-#        print("  %s = %s" % (desc, value))
+# if user defined 00:00:00:00:00:00 as MAC adress, we try to scan for it
+if(g_strMACAddress == "00:00:00:00:00:00"):
+    scanner = Scanner()
+    devices = scanner.scan(2.0)
+    devaddr = 0
 
-if(devaddr != 0):
- 
+    for dev in devices:
+    #    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        devName = dev.getValueText(9)
+        if devName  == "dani" :
+            print("found dani device with addr: " + dev.addr)
+            devaddr = dev.addr
+    #    for (adtype, desc, value) in dev.getScanData():
+    #        print("  %s = %s" % (desc, value))
+
+    if(devaddr != 0):
+        g_strMACAddress = devaddr
+        
+if(g_strMACAddress != "00:00:00:00:00:00"):
     #temp_uuid = UUID(0x2221)
     temp_uuid = UUID("D3EDE59D-0CEE-433D-85AD-81B40BC51C3E")
     temp_uuidwr = UUID("54ED5C0A-8E0C-48D8-803C-7D20CE52DCA7")
@@ -66,12 +83,15 @@ if(devaddr != 0):
                     strval = ch.read()
                     print "read: " + strval
                     val = binascii.b2a_hex(strval)
-                    g_val = binascii.unhexlify(val)
+                    g_intVal = binascii.unhexlify(val)
                 
                     chwrite.write(g_cobid)
     #               val = struct.unpack('f', val)[0]
 #                    print str(val)                    
-                    exit_code = 1
+                    g_exit_code = 1
+                    f = open('/var/www/html/openWB/ramdisk/soc', 'w')
+                    f.write(str(g_intVal))
+                    f.close()
                 except:
                     print("Oops!",sys.exc_info()[0],"occured.")
 
@@ -79,9 +99,5 @@ if(devaddr != 0):
         print("Oops!",sys.exc_info()[0],"occured.")
         p.disconnect()
 
-
-f = open('/var/www/html/openWB/ramdisk/soc', 'w')
-f.write(str(g_val))
-f.close()
-sys.exit(exit_code)
+sys.exit(g_exit_code)
 
