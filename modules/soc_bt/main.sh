8cf550103355f9180c5e35946472a89896ca1dd8
nikigraus <dani@reislimann.ch>
2019-10-22 20:46:55 +0200

    Added timeout

--- modules/soc_bt/main.sh
+++ modules/soc_bt/main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /var/www/html/openWB/openwb.conf
 
-sudo python /var/www/html/openWB/modules/soc_bt/socbt.py $socbt_mac $socbt_cobid $socbt_byteno $socbt_offset $socbt_factor &
+timeout 6 python /var/www/html/openWB/modules/soc_bt/socbt.py $socbt_mac $socbt_cobid $socbt_byteno $socbt_offset $socbt_factor &
