3b2f6bee11aaf942c37a5355d6dbec5d8e0351b2
nikigraus <dani@reislimann.ch>
2019-10-27 08:40:44 +0100

    Update main.sh

--- modules/soc_bt/main.sh
+++ modules/soc_bt/main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 . /var/www/html/openWB/openwb.conf
 
+sudo hciconfig hci0 reset
 timeout 6 python /var/www/html/openWB/modules/soc_bt/socbt.py $socbt_mac $socbt_cobid $socbt_byteno $socbt_offset $socbt_factor &
+
